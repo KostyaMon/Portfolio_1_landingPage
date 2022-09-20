0a228af1ab0641e34fd2873b30e4552bc047aca5
KostyaM <graffkost@mail.ru>
2022-09-20 05:46:43 +0300

    Fix some bugs

--- js/common.js
+++ js/common.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
         $(".header__burger, .header__menu").toggleClass("active");
         $("body").toggleClass("lock");
     });
+     $(".header__link").click(function (event) {
+         $(".header__burger, .header__menu").removeClass("active");
+         $("body").removeClass("lock");
+    });
 });
 $('.parallaxie').parallaxie({
     speed: 0.4,
@@ -16,30 +20,3 @@ const swiper = new Swiper('.swiper', {
         clickable: true,
     },
 });
-
-// window.onload = function () {
-//     const parallax = document.querySelector('.parallax')
-//     if (parallax) {
-//         //  Parallax scroll
-//         const content = document.querySelector('.parallax__container')
-//         const header = document.querySelector('.header')
-//         let thresholdSets = [];
-//         for (let i = 0; i <= 1.0; i+= 0.005) {
-//             thresholdSets.push(i);
-//         }
-//         const callback = function (entries, observer) {
-//             const scrollTopProcent = window.pageYOffset / parallax.offsetHeight * 100;
-//             setParallaxItemsStyle(scrollTopProcent)
-//         };
-//         const observer = new IntersectionObserver(callback, {
-//             threshold: thresholdSets
-//         });
-//
-//         observer.observe(document.querySelector('.content'));
-//
-//         function setParallaxItemsStyle(scrollTopProcent) {
-//             content.style.cssText = `transform: translate(0%, -${scrollTopProcent / 9}%); `
-//             header.style.cssText = `transform: translate(0%, -${scrollTopProcent / 3}%); `
-//         }
-//     }
-// }
