cd0cd23b36569d24e0a20b0de85c05b3e9bcb100
KostyaM <graffkost@mail.ru>
2022-09-21 02:55:28 +0300

    Improved adaptive adaptability

--- js/common.js
+++ js/common.js
@@ -1,20 +1,18 @@
 $(document).ready(function () {
-    $(".header__burger").click(function (event) {
+    $(".header__burger").click(function () {
         $(".header__burger, .header__menu").toggleClass("active");
         $("body").toggleClass("lock");
     });
-     $(".header__link").click(function (event) {
+     $(".header__link").click(function () {
          $(".header__burger, .header__menu").removeClass("active");
          $("body").removeClass("lock");
     });
-});
-$('.parallaxie').parallaxie({
+     $('.parallaxie').parallaxie({
     speed: 0.4,
-    size: 'cover',
+    });
 });
 
 const swiper = new Swiper('.swiper', {
-    // loop: true,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
